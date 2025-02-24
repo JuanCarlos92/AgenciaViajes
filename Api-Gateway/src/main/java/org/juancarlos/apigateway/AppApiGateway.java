a34b69c93859aaec32ee1c821d89048fdb3fd9fe
Juan Carlos <juancarlosfilter@gmail.com>
2025-02-24 12:26:10 +0100

    api gateway

--- Api-Gateway/src/main/java/org/juancarlos/apigateway/AppApiGateway.java
+++ Api-Gateway/src/main/java/org/juancarlos/apigateway/AppApiGateway.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApiGatewayApplication {
+public class AppApiGateway {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication.run(AppApiGateway.class, args);
     }
 
 }
