e5f5a47115232154d7b3f52b5e9c36ef9b368372
Juan Carlos <juancarlosfilter@gmail.com>
2025-03-02 19:04:08 +0100

    create folder backend

--- backend/Api-Gateway/src/main/java/org/juancarlos/apigateway/SecurityConfig.java
+++ backend/Api-Gateway/src/main/java/org/juancarlos/apigateway/SecurityConfig.java
@@ -3,10 +3,11 @@ package org.juancarlos.apigateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,23 +19,28 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity // Habilita Spring Security
 public class SecurityConfig {
 
-    public DataSource dataSource() {
-        DriverManagerDataSource ds=new DriverManagerDataSource();
-        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/springsecurity?serverTimezone=UTC");
-        ds.setUsername("root");
-        ds.setPassword("root");
-        return ds;
+    //        public DataSource dataSource() {
+//        DriverManagerDataSource ds=new DriverManagerDataSource();
+//        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        ds.setUrl("jdbc:mysql://localhost:3306/springsecurity?serverTimezone=UTC");
+//        ds.setUsername("root");
+//        ds.setPassword("root");
+//        return ds;
+//    }
+
+
+    private final DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     //config de users de la BD
     @Bean
     public JdbcUserDetailsManager usersDetailsJdbc() {
-        JdbcUserDetailsManager jdbcDetails = new JdbcUserDetailsManager(dataSource());
-        jdbcDetails.setUsersByUsernameQuery("select user, pwd, enabled"
-                + " from users where user=?");
-        jdbcDetails.setAuthoritiesByUsernameQuery("select user, rol "
-                + "from roles where user=?");
+        JdbcUserDetailsManager jdbcDetails = new JdbcUserDetailsManager(dataSource);
+        jdbcDetails.setUsersByUsernameQuery("SELECT user, pwd, enabled FROM users WHERE user=?");
+        jdbcDetails.setAuthoritiesByUsernameQuery("SELECT user, rol  FROM roles WHERE user=?");
         return jdbcDetails;
     }
 
@@ -43,17 +49,12 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         //--------------------------------- SECURITY HOTELES --------------------------------
-                        .requestMatchers(HttpMethod.GET, "/hoteles").hasAuthority("ROLE_GUEST")
-                        .requestMatchers(HttpMethod.GET, "/hoteles//{id}").hasAuthority("ROLE_GUEST")
-                        .requestMatchers(HttpMethod.GET, "/nombre/{nombre}").hasAuthority("ROLE_GUEST")
+                        .requestMatchers(HttpMethod.GET, "/hoteles").hasAuthority("ROLE_ADMIN")
                         .requestMatchers(HttpMethod.POST, "/hoteles").hasAuthority("ROLE_ADMIN")
                         .requestMatchers(HttpMethod.POST, "/hoteles/{id}/{disponible}").hasAuthority("ROLE_USER")
                         .requestMatchers(HttpMethod.PUT, "/hoteles").hasAuthority("ROLE_OPERATOR")
                         .requestMatchers(HttpMethod.DELETE, "/hoteles/{id}").hasAuthority("ROLE_ADMIN")
                         //--------------------------------- SECURITY VUELOS --------------------------------
-                        .requestMatchers(HttpMethod.GET, "/vuelos").hasAuthority("ROLE_GUEST")
-                        .requestMatchers(HttpMethod.GET, "/vuelos//{id}").hasAuthority("ROLE_GUEST")
-                        .requestMatchers(HttpMethod.GET, "/nombre/{nombre}").hasAuthority("ROLE_GUEST")
                         .requestMatchers(HttpMethod.POST, "/vuelos").hasAuthority("ROLE_ADMIN")
                         .requestMatchers(HttpMethod.POST, "/vuelos/{id}/{disponible}").hasAuthority("ROLE_USER")
                         .requestMatchers(HttpMethod.PUT, "/vuelos").hasAuthority("ROLE_OPERATOR")
@@ -61,14 +62,20 @@ public class SecurityConfig {
                         //--------------------------------- SECURITY RESERVAS --------------------------------
                         .requestMatchers(HttpMethod.POST, "/reservas").hasAuthority("ROLE_USER")
                         .requestMatchers(HttpMethod.GET, "/reservas").hasAuthority("ROLE_USER")
-                        .requestMatchers(HttpMethod.GET, "/reservas//{id}").hasAuthority("ROLE_USER")
+                        .requestMatchers(HttpMethod.GET, "/reservas/{id}").hasAuthority("ROLE_USER")
                         .requestMatchers(HttpMethod.GET, "/nombre/{nombre}").hasAuthority("ROLE_USER")
                         .requestMatchers(HttpMethod.DELETE, "/reservas/{id}").hasAuthority("ROLE_ADMIN")
                         .anyRequest().permitAll()  // (cualquier otra URL) --> LIBRE SIN RESTRICTION
                 )
                 .httpBasic(withDefaults());  // Configurar authentication basic
-
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
 }
+
